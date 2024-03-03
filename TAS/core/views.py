9466f9b7343636d36ee9a512bfea6e9a4ffb32b1
Nkisi <nkisi@pop-os.localdomain>
2024-03-03 14:33:01 -0500

    Add preview.pdf to unrestricted server resources. Add 'Read Preview' button to index.html

--- TAS/core/views.py
+++ TAS/core/views.py
@@ -1,5 +1,16 @@
+#Core Django
 from django.shortcuts import render
+from django.http import FileResponse
+
+#Python 
+
+#Third parties 
+
 
 # Create your views here.
 def index(request):
-	return render(request, "core/index.html")
+	return render(request, "core/index.html") 
+
+def view_preview(request):
+	if request.method == "GET":
+		return FileResponse(open('core/static/preview/TheAfricaStory_preview_pdf.pdf', 'rb'), content_type='application/pdf')
